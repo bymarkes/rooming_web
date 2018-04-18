87d438d96b25cdb887908cec89c27a3493cedb67
barbosound <barbo_94@hotmail.com>
2018-04-18 17:52:43 +0200

    change return result JSON index.js

--- rooming.js
+++ rooming.js
@@ -7,114 +7,128 @@ function postCategoria(JSON){ // POST
 
 	var httpRequest = new XMLHttpRequest();
 
+	var result;
+
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"categoria", true);
+	httpRequest.open('POST', URL+"categoria", false);
 
 	httpRequest.send(JSON);
+
+	return result;
 }
 
 function getAllCategoria(){ // GET ALL Categoria
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"categoria", true);
+	httpRequest.open('GET', URL+"categoria", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putCategoria(id, JSON){ // PUT
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"categoria/"+id, true);
+	httpRequest.open('PUT', URL+"categoria/"+id, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function getCategoria(id){ //GET SHOW
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"categoria/"+id, true);
+	httpRequest.open('GET', URL+"categoria/"+id, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function deleteCategoria(id){ //DELETE
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"categoria/"+id, true);
+	httpRequest.open('DELETE', URL+"categoria/"+id, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getCategoriaRoom(id){ //GET Rooms from Categoria
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"categoria/"+id+"/room", true);
+	httpRequest.open('GET', URL+"categoria/"+id+"/room", false);
 
 	httpRequest.send();
+	return result;
 }
 /** END CATEGORIA **/
 
@@ -123,210 +137,232 @@ function getCategoriaRoom(id){ //GET Rooms from Categoria
 function getAllEstabliment(){ // GET All Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"establiment", true);
+	httpRequest.open('GET', URL+"establiment", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function postEstabliment(JSON){ // POST
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"establiment", true);
+	httpRequest.open('POST', URL+"establiment", false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function getEstabliment(id){ // GET SHOW
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"establiment/"+id, true);
+	httpRequest.open('GET', URL+"establiment/"+id, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putEstabliment(JSON){ // PUT
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"establiment", true);
+	httpRequest.open('PUT', URL+"establiment", false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function deleteEstabliment(id){ // DELETE
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"establiment/"+id, true);
+	httpRequest.open('DELETE', URL+"establiment/"+id, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getEstablimentFoto(id){ // GET FOTOS from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"establiment/"+id+"/foto", true);
+	httpRequest.open('GET', URL+"establiment/"+id+"/foto", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getEstablimentGps(id){ // GET GPS from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"establiment/"+id+"/gps", true);
+	httpRequest.open('GET', URL+"establiment/"+id+"/gps", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function postEstablimentGps(id){ // GET GPS from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"establiment/"+id+"/gps", true);
+	httpRequest.open('POST', URL+"establiment/"+id+"/gps", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putEstablimentGps(idEstabliment, idGPS){ // PUT GPS from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"establiment/"+idEstabliment+"/gps/"+idGPS, true);
+	httpRequest.open('PUT', URL+"establiment/"+idEstabliment+"/gps/"+idGPS, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function deleteEstablimentGps(idEstabliment, idGPS){ // DELETE GPS from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"establiment/"+idEstabliment+"/gps/"+idGPS, true);
+	httpRequest.open('DELETE', URL+"establiment/"+idEstabliment+"/gps/"+idGPS, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getEstablimentRooms(id){ // GET Rooms from Establiment
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"establiment/"+id+"/room", true);
+	httpRequest.open('GET', URL+"establiment/"+id+"/room", false);
 
 	httpRequest.send();
+	return result;
 }
 /** END ESTABLIMENT**/
 
@@ -336,96 +372,106 @@ function getEstablimentRooms(id){ // GET Rooms from Establiment
 function getAllFoto(){ // GET ALL FOTO 
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"foto", true);
+	httpRequest.open('GET', URL+"foto", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function postFoto(JSON){ // POST FOTO 
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"foto", true);
+	httpRequest.open('POST', URL+"foto", false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function getFoto(id){ // GET FOTO show
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"foto/"+id, true);
+	httpRequest.open('GET', URL+"foto/"+id, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function putFoto(id, JSON){ // PUT FOTO 
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"foto/"+id, true);
+	httpRequest.open('PUT', URL+"foto/"+id, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function deleteFoto(id){ // DELETE FOTO show
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"foto/"+id, true);
+	httpRequest.open('DELETE', URL+"foto/"+id, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 /** END FOTO **/
@@ -459,287 +505,317 @@ function getAllRoom(){ //GET ALL ROOMS
 function postRoom(){ //POST ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"room", true);
+	httpRequest.open('POST', URL+"room", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getRoom(id){ // GET ROOM ID
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id, true);
+	httpRequest.open('GET', URL+"room/"+id, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putRoom(id, JSON){
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id, true);
+	httpRequest.open('GET', URL+"room/"+id, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function deleteRoom(id){
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"room", true);
+	httpRequest.open('DELETE', URL+"room", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getRoomAllComentari(id){ //GET ALL COMENTARIS from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id+"/comentari", true);
+	httpRequest.open('GET', URL+"room/"+id+"/comentari", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function postRoomComentari(id, JSON){ //POST COMENTARIS from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"room/"+id+"/comentari", true);
+	httpRequest.open('POST', URL+"room/"+id+"/comentari", false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function getRoomComentari(idRoom, idComentari){ //GET id COMENTARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+idRoom+"/comentari/"+idComentari, true);
+	httpRequest.open('GET', URL+"room/"+idRoom+"/comentari/"+idComentari, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putRoomComentari(idRoom, idComentari, JSON){ //PUT id COMENTARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"room/"+idRoom+"/comentari/"+idComentari, true);
+	httpRequest.open('PUT', URL+"room/"+idRoom+"/comentari/"+idComentari, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function deleteRoomComentari(idRoom, idComentari){ //DELETE id COMENTARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"room/"+idRoom+"/comentari/"+idComentari, true);
+	httpRequest.open('DELETE', URL+"room/"+idRoom+"/comentari/"+idComentari, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getRoomAllFoto(id){ //GET ALL FOTOS from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id+"/foto", true);
+	httpRequest.open('GET', URL+"room/"+id+"/foto", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getRoomAllHorari(id){ //GET ALL HORARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id+"/horari", true);
+	httpRequest.open('GET', URL+"room/"+id+"/horari", false);
 
 	httpRequest.send();
+	return result;
 }
 
 
 function postRoomHorari(id){ //POST HORARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('POST', URL+"room/"+id+"/horari", true);
+	httpRequest.open('POST', URL+"room/"+id+"/horari", false);
 
 	httpRequest.send();
+	return result;
 }
 
 function getRoomHorari(idRoom, idHorari){ //GET id HORARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('GET', URL+"room/"+id+"/horari/"+idHorari, true);
+	httpRequest.open('GET', URL+"room/"+id+"/horari/"+idHorari, false);
 
 	httpRequest.send();
+	return result;
 }
 
 function putRoomHorari(idRoom, idHorari, JSON){ //PUT id HORARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('PUT', URL+"room/"+id+"/horari/"+idHorari, true);
+	httpRequest.open('PUT', URL+"room/"+id+"/horari/"+idHorari, false);
 
 	httpRequest.send(JSON);
+	return result;
 }
 
 function getRoomHorari(idRoom, idHorari){ //DELETE id HORARI from ROOM
 
 	var httpRequest = new XMLHttpRequest();
+	var result;
 
 	httpRequest.onreadystatechange = function (){
 
 		if (httpRequest.readyState == 4 && httpRequest.status == 200 ) {
 
 			var jsonParsed = JSON.parse(httpRequest.responseText);
-			return jsonParsed;
+			result = jsonParsed;
 		}
 
 	};
 
-	httpRequest.open('DELETE', URL+"room/"+id+"/horari/"+idHorari, true);
+	httpRequest.open('DELETE', URL+"room/"+id+"/horari/"+idHorari, false);
 
 	httpRequest.send();
+	return result;
 }
 
 /** END ROOM **/
