d930890d0105995e9ef1667e3c510af446c92046
bymarkes <davidmarques.dp@gmail.com>
2018-04-18 20:14:29 +0100

    Functionality on categories, rooms, categoria

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.get('/login', function(req,res){
 });
 
 app.get('/categories', function(req,res){
-    res.render('categories');
+    var c = roomingApi.getAllCategoria();
+    res.render('categories',{'categories':c});
+});
+
+app.get('/categories/:id', function(req,res){
+    var c = roomingApi.getCategoria(req.params.id);
+    var r = roomingApi.getCategoriaRoom(req.params.id);    
+    res.render('categoria',{'categoria':c, 'rooms':r});
 });
 
 app.get('/register', function(req,res){
@@ -33,7 +40,9 @@ app.get('/plantilla', function(req,res){
 });
 
 app.get('/rooms', function(req,res){
-    res.render('room');
+    var r = roomingApi.getAllRoom();  
+    var f = roomingApi.getAllFoto();      
+    res.render('rooms',{'rooms':r, 'fotos':f});
 });
 
 app.get('/profile', function(req,res){
